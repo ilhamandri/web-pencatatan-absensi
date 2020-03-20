e2ce12bbd512ba657cf659282c5bef643230678f
kevinrizkhy <kevin.rizkhy85@gmail.com>
2020-03-20 10:44:19 +0700

    add matkul + ruang info in response

--- get_qr.php
+++ get_qr.php
@@ -13,20 +13,25 @@
 	$token = $data["token"];
 	$id_ruang = $data["id_ruang"];
 
-	$jam = 7;
-	$sql = "SELECT code, id FROM jadwal WHERE ruang_id = $id_ruang AND jam_mulai < $jam AND jam_selesai > $jam";
+	$jam = 9;
+	$sql = "SELECT code, jadwal.id AS id_jadwal, jam_mulai, jam_selesai, matakuliah.nama AS nama_matkul, matakuliah.kode AS kode_matkul, ruang.nama AS nama_ruang FROM jadwal JOIN  matakuliah ON matakuliah.id = jadwal.matakuliah_id JOIN ruang on ruang.id = jadwal.ruang_id WHERE ruang_id = $id_ruang AND jam_mulai <= $jam AND jam_selesai >= $jam";
 	$result = mysqli_query($conn, $sql);
-	$matkul_arr = array();
+	$data = array();
 	$new_token = "";
 	$curr_token = "";
 	if (mysqli_num_rows($result) > 0) {
 	    while($row = mysqli_fetch_assoc($result)) {
 	    	$curr_token = $row["code"];
-	    	$id = $row["id"];
+	    	$id = $row["id_jadwal"];
 	    	if($curr_token != $token){
 	    		$token = md5($id_ruang.time());
 	    		$sql = "UPDATE jadwal SET code = '$token' WHERE id=$id";
 			    if ($conn->query($sql) === TRUE){
+			    	$data = array(
+			        	"kode" => $row["kode_matkul"],
+			        	"nama" => $row["nama_matkul"],
+			        	"ruang" => $row["nama_ruang"]
+			        );
 			    	$new_token = $token;
 			    }
 			}else{
@@ -40,7 +45,8 @@
 	if($new_token != ""){
 		echo json_encode(array(
     		"STATUS_CODE" => "OK",
-			"token" => $new_token
+			"token" => $new_token,
+			"data" => $data
 		));
 	}else{
 		echo json_encode(array(
